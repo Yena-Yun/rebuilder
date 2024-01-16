2cf90c732ec2e543dad289ac12de075290fb2e17
Yena-Yun <cheryleduart@gmail.com>
2024-01-16 17:19:08 +0900

    setting: svg, png, mp4 타입스크립트 설정

---
+++ assets.d.ts
@@ -0,0 +1,9 @@
+declare module '*.png';
+declare module '*.mp4';
+declare module '*.svg' {
+  import React = require('react');
+
+  export const ReactComponent: REact.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}

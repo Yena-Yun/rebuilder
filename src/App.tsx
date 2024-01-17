63d7d85157982cc0d275d87ab9735a525d82f169
Yena-Yun <cheryleduart@gmail.com>
2024-01-17 15:14:26 +0900

    feat: App에 Header 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Header } from './components/Header/Header';
+
 function App() {
-  return <div className='App'>룰루</div>;
+  return (
+    <div className='App'>
+      <Header />
+    </div>
+  );
 }
 
 export default App;

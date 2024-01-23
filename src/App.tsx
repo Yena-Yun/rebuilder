41dda10ec60ecc5db79300c04bba166d3e6251b8
Yena-Yun <cheryleduart@gmail.com>
2024-01-23 15:46:44 +0900

    feat: App에 Section 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Header } from './components/Header/Header';
 import { Section1 } from './components/Section1/Section1';
+import { Section2 } from './components/Section2/Section2';
+import { Section3 } from './components/Section3/Section3';
 
 function App() {
   return (
     <>
       <Header />
       <Section1 />
+      <Section2 />
+      <Section3 />
     </>
   );
 }

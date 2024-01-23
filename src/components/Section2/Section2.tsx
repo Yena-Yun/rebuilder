9b20c7a0c3a046ec2caddddaed92a991d99f74c5
Yena-Yun <cheryleduart@gmail.com>
2024-01-23 17:34:58 +0900

    refactor: 스타일 컴포넌트 모듈화, css helper로 animation 모듈화

--- src/components/Section2/Section2.tsx
+++ src/components/Section2/Section2.tsx
@@ -1,4 +1,5 @@
-import styled, { keyframes } from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
+import { WORD_LIST } from './constants';
 
 export const Section2 = () => {
   return (
@@ -7,31 +8,24 @@ export const Section2 = () => {
         <FlexColumn>
           <FlexBetween>
             <div></div>
-            <ParagraphContainer>
-              <Paragraph1Box>
-                <Paragraph1>
-                  <span>V</span>aluable
-                </Paragraph1>
-              </Paragraph1Box>
-              <Paragraph2Box>
-                <Paragraph2>
-                  <span>R</span>are
-                </Paragraph2>
-              </Paragraph2Box>
-              <Paragraph3Box>
-                <Paragraph3>
-                  <span>I</span>nimitable
-                </Paragraph3>
-              </Paragraph3Box>
-              <Paragraph4Box>
-                <Paragraph4>
-                  <span>N</span>on - substitutable
-                </Paragraph4>
-              </Paragraph4Box>
-            </ParagraphContainer>
+            <WordContainer>
+              {WORD_LIST.map(({ id, fullWord, capital, restWord }) => (
+                <WordBox key={id} $fullWord={fullWord}>
+                  <Word>
+                    <span>{capital}</span>
+                    {restWord}
+                  </Word>
+                </WordBox>
+              ))}
+            </WordContainer>
             <div></div>
           </FlexBetween>
-          <Space></Space>
+
+          <SentenceContainer>
+            <span>
+              VRIN은 대체 불가능한 차세대 3D 비전 AI로 발전하고 있습니다.
+            </span>
+          </SentenceContainer>
         </FlexColumn>
       </InnerContainer>
     </Container>
@@ -43,7 +37,7 @@ const Container = styled.div`
   width: 100%;
   height: 90vh;
   background: rgb(0, 0, 0);
-  overflow: hidden;
+  /* overflow: hidden; */
 `;
 
 const InnerContainer = styled.div`
@@ -75,43 +69,34 @@ const easeinout = keyframes`
     }
 `;
 
-const Space = styled.div`
-  animation: 1s ease-in-out 2s 1 normal forwards running ${easeinout};
-  opacity: 0;
-  margin-top: 204px;
-  text-align: center;
-`;
-
-const ParagraphContainer = styled.div`
+const WordContainer = styled.div`
   display: flex;
   flex-direction: column;
 `;
 
-const Paragraph1Box = styled.div`
-  animation: 1s ease-in-out 0s 1 normal forwards running ${easeinout};
+const WordBox = styled.div<{ $fullWord: string }>`
   opacity: 0;
   margin-bottom: 4.6px;
+
+  ${({ $fullWord }) =>
+    $fullWord === 'Valuable'
+      ? css`
+          animation: 1s ease-in-out 0.5s 1 normal forwards running ${easeinout};
+        `
+      : $fullWord === 'Rare'
+      ? css`
+          animation: 1s ease-in-out 1s 1 normal forwards running ${easeinout};
+        `
+      : $fullWord === 'Inimitable'
+      ? css`
+          animation: 1s ease-in-out 1.5s 1 normal forwards running ${easeinout};
+        `
+      : css`
+          animation: 1s ease-in-out 2s 1 normal forwards running ${easeinout};
+        `};
 `;
 
-const Paragraph2Box = styled.div`
-  animation: 1s ease-in-out 0.5s 1 normal forwards running ${easeinout};
-  opacity: 0;
-  margin-bottom: 4.6px;
-`;
-
-const Paragraph3Box = styled.div`
-  animation: 1s ease-in-out 1s 1 normal forwards running ${easeinout};
-  opacity: 0;
-  margin-bottom: 4.6px;
-`;
-
-const Paragraph4Box = styled.div`
-  animation: 1s ease-in-out 1.5s 1 normal forwards running ${easeinout};
-  opacity: 0;
-  margin-bottom: 4.6px;
-`;
-
-const Paragraph1 = styled.span`
+const Word = styled.span`
   color: rgb(255, 255, 255);
   font-size: 5.06rem;
   font-weight: 500;
@@ -124,41 +109,18 @@ const Paragraph1 = styled.span`
   }
 `;
 
-const Paragraph2 = styled.span`
-  color: rgb(255, 255, 255);
-  font-size: 5.06rem;
-  font-weight: 500;
-  line-height: 138%;
+const SentenceContainer = styled.div`
+  animation: 1s ease-in-out 2.5s 1 normal forwards running ${easeinout};
+  opacity: 0;
+  margin-top: 204px;
   text-align: center;
 
   & span {
-    font-weight: 700;
-    font-size: 6.34rem;
-  }
-`;
-
-const Paragraph3 = styled.span`
-  color: rgb(255, 255, 255);
-  font-size: 5.06rem;
-  font-weight: 500;
-  line-height: 138%;
-  text-align: center;
-
-  & span {
-    font-weight: 700;
-    font-size: 6.34rem;
-  }
-`;
-
-const Paragraph4 = styled.span`
-  color: rgb(255, 255, 255);
-  font-size: 5.06rem;
-  font-weight: 500;
-  line-height: 138%;
-  text-align: center;
-
-  & span {
-    font-weight: 700;
-    font-size: 6.34rem;
+    color: rgb(255, 255, 255);
+    font-size: 3.6rem;
+    font-weight: 500;
+    line-height: 138%;
+    text-align: center;
+    white-space: pre-wrap;
   }
 `;
---
+++ src/components/Section2/constants/index.ts
@@ -0,0 +1,26 @@
+export const WORD_LIST = [
+  {
+    id: 0,
+    fullWord: 'Valuable',
+    capital: 'V',
+    restWord: 'aluable',
+  },
+  {
+    id: 1,
+    fullWord: 'Rare',
+    capital: 'R',
+    restWord: 'are',
+  },
+  {
+    id: 2,
+    fullWord: 'Inimitable',
+    capital: 'I',
+    restWord: 'nimitable',
+  },
+  {
+    id: 3,
+    fullWord: 'Non-substitutable',
+    capital: 'N',
+    restWord: 'on - substitutable',
+  },
+];

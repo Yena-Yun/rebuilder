34e32eca5561014993fffa1ce1fa09553fc7e135
Yena-Yun <cheryleduart@gmail.com>
2024-01-25 16:40:37 +0900

    feat: 글자 떠오르는 애니메이션에 IntersectionObserver 추가

--- src/components/Section2/Section2.tsx
+++ src/components/Section2/Section2.tsx
@@ -1,14 +1,52 @@
+import { useEffect, useRef, useState } from 'react';
 import styled, { css, keyframes } from 'styled-components';
 import { WORD_LIST } from './constants';
 
 export const Section2 = () => {
+  const scrollRef = useRef<HTMLDivElement | null>(null);
+  const [isInViewport, setIsInViewport] = useState(false);
+
+  useEffect(() => {
+    // 요소가 아직 준비되지 않은 경우 중단
+    if (!scrollRef.current) return;
+
+    const callback = (entries: IntersectionObserverEntry[]) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          // 요소가 뷰포트에 나타난 경우
+          setIsInViewport(true);
+        } else {
+          // 요소가 뷰포트를 벗어난 경우
+          setIsInViewport(false);
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(callback, {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0,
+    });
+
+    // 요소 관찰 시작
+    observer.observe(scrollRef.current);
+
+    // 컴포넌트가 언마운트되면 관찰 중단
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
   return (
     <Container>
       <InnerContainer>
         <FlexColumn>
           <FlexBetween>
             <div></div>
-            <WordContainer>
+            <WordContainer
+              ref={scrollRef}
+              className={isInViewport ? 'frame-in' : ''}
+            >
               {WORD_LIST.map(({ id, fullWord, capital, restWord }) => (
                 <WordBox key={id} $fullWord={fullWord}>
                   <Word>
@@ -72,6 +110,10 @@ const easeinout = keyframes`
 const WordContainer = styled.div`
   display: flex;
   flex-direction: column;
+
+  &.frame-in {
+    animation: ${easeinout} 2s forwards;
+  }
 `;
 
 const WordBox = styled.div<{ $fullWord: string }>`

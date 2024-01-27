d518afa99ac86c0377e2251dda2faa515b46a093
Yena-Yun <cheryleduart@gmail.com>
2024-01-27 16:43:38 +0900

    move: 섹션 컴포넌트 Technology 폴더로 이동

--- src/pages/Technology.tsx
+++ src/pages/Technology.tsx
@@ -1,6 +1,6 @@
-import { Section1 } from '../components/Section1/Section1';
-import { Section2 } from '../components/Section2/Section2';
-import { Section3 } from '../components/Section3/Section3';
+import { Section1 } from '../components/Technology/Section1/Section1';
+import { Section2 } from '../components/Technology/Section2/Section2';
+import { Section3 } from '../components/Technology/Section3/Section3';
 
 export const Technology = () => {
   return (

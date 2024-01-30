d05b89068e38f41845f34c37a7941533241c38f2
Yena-Yun <cheryleduart@gmail.com>
2024-01-30 16:17:03 +0900

    refactor: useIntersectionObserver 훅 분리

--- src/components/Technology/Section2/Section2.tsx
+++ src/components/Technology/Section2/Section2.tsx
@@ -1,38 +1,10 @@
-import { useEffect, useRef, useState } from 'react';
 import styled, { keyframes } from 'styled-components';
+import { useIntersectionObserver } from '../../../hooks/useIntersectionObserver';
 
 export const Section2 = () => {
-  const scrollRef = useRef<HTMLDivElement | null>(null);
-  const [isInViewport, setIsInViewport] = useState(false);
-
-  useEffect(() => {
-    // 요소가 아직 준비되지 않은 경우 중단
-    if (!scrollRef.current) return;
-
-    const callback = (entries: IntersectionObserverEntry[]) => {
-      entries.forEach((entry) => {
-        // 요소가 뷰포트에 나타난 경우
-        if (entry.isIntersecting) {
-          setIsInViewport(true);
-          observer.unobserve(entry.target);
-        }
-      });
-    };
-
-    const observer = new IntersectionObserver(callback, {
-      root: null,
-      rootMargin: '400px',
-      threshold: 0,
-    });
-
-    // 요소 관찰 시작
-    observer.observe(scrollRef.current);
-
-    // 컴포넌트가 언마운트되면 관찰 중단
-    return () => {
-      observer.disconnect();
-    };
-  }, []);
+  const { scrollRef, isInViewport } = useIntersectionObserver({
+    rootMargin: '500px',
+  });
 
   return (
     <Container>

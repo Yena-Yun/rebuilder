a5a88d53a8f3de7ddba30c42aec266b667af586d
Yena-Yun <cheryleduart@gmail.com>
2024-02-12 17:24:30 +0900

    feat: Technology 페이지에 섹션 5, 6, 7 추가

--- src/pages/Technology.tsx
+++ src/pages/Technology.tsx
@@ -2,6 +2,9 @@ import { Section1 } from '../components/Technology/Section1/Section1';
 import { Section2 } from '../components/Technology/Section2/Section2';
 import { Section3 } from '../components/Technology/Section3/Section3';
 import { Section4 } from '../components/Technology/Section4/Section4';
+import { Section5 } from '../components/Technology/Section5/Section5';
+import { Section6 } from '../components/Technology/Section6/Section6';
+import { Section7 } from '../components/Technology/Section7/Section7';
 
 export const Technology = () => {
   return (
@@ -10,6 +13,9 @@ export const Technology = () => {
       <Section2 />
       <Section3 />
       <Section4 />
+      <Section5 />
+      <Section6 />
+      <Section7 />
     </>
   );
 };

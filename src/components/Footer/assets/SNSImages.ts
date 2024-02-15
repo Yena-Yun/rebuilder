77fc329664f1e0003cd2b2e1c5593a0872227e4a
Yena-Yun <cheryleduart@gmail.com>
2024-02-15 15:17:49 +0900

    chore: (파일 이동 이후) import문 코드 수정

--- src/components/Footer/assets/SNSImages.ts
+++ src/components/Footer/assets/SNSImages.ts
@@ -1,6 +1,6 @@
-import InstagramImage from '../../../assets/svgs/footer/instagram.svg';
-import LinkedinImage from '../../../assets/svgs/footer/linkedin.svg';
-import NaverBlogImage from '../../../assets/svgs/footer/naver-blog.svg';
-import TistoryImage from '../../../assets/svgs/footer/tistory.svg';
+import InstagramImage from '../../../assets/svgs/footer/SNSIcons/instagram.svg';
+import LinkedinImage from '../../../assets/svgs/footer/SNSIcons/linkedin.svg';
+import NaverBlogImage from '../../../assets/svgs/footer/SNSIcons/naver-blog.svg';
+import TistoryImage from '../../../assets/svgs/footer/SNSIcons/tistory.svg';
 
 export { InstagramImage, LinkedinImage, NaverBlogImage, TistoryImage };

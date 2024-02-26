2d8aa23c905b7c52d72bc762a24a959fd1adb23a
Yena-Yun <cheryleduart@gmail.com>
2024-02-26 20:41:55 +0900

    refactor: 하위 constants를 상위에 하나로 합침

--- src/components/Header/LargerHeader/constants/index.ts
+++
@@ -1,9 +0,0 @@
-import { MENUS } from '../../shared/constants';
-
-export const TECH = MENUS[1];
-
-export const LANGS = ['KOR', 'ENG'];
-
-export const KOR = LANGS[0];
-
-export const TECH_MENUS = ['광원추론', '재질추론', '실측크기', '3D 공간 영상'];
---
+++ src/components/Header/constants/index.ts
@@ -0,0 +1,18 @@
+export const LANGUAGES = [
+  {
+    code: 'ko',
+    display: 'KOR',
+  },
+  {
+    code: 'en',
+    display: 'ENG',
+  },
+];
+
+export const KOR = LANGUAGES[0].display;
+
+export const MENUS = ['Service', 'Technology', 'About', 'Contact'];
+
+export const TECHNOLOGY = MENUS[1];
+
+export const TECH_MENUS = ['광원추론', '재질추론', '실측크기', '3D 공간 영상'];

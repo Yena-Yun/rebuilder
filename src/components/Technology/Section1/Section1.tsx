0235fe23e4d40e3c2ed90530fe841a0288824c82
Yena-Yun <cheryleduart@gmail.com>
2024-02-27 15:52:57 +0900

    style: 섹션1 영상 소스 반응형 적용, theme 적용

--- src/components/Technology/Section1/Section1.tsx
+++ src/components/Technology/Section1/Section1.tsx
@@ -1,33 +1,34 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { useTranslation } from 'react-i18next';
+import { easeInOut } from '../keyframeStyle';
+import { useMedia } from 'utils/Query';
 import { FlexColumnCenter } from 'styles/flex';
-
-const VIDEO_SOURCE = '/videos/pc/tech_video1_pc.mp4';
-const VIDEO_TYPE = 'video/mp4';
+import { Keyframes } from 'styled-components/dist/types';
 
 export const Section1 = () => {
   const { t } = useTranslation();
 
+  const { isMobile, isTabletS } = useMedia();
+
+  const VIDEO_SOURCE = () => {
+    const media = isMobile ? 'mobile' : isTabletS ? 'tablet' : 'pc';
+    return `/videos/${media}/tech_video1_${media}.mp4`;
+  };
+
   return (
     <Container>
-      <IntroVideo>
-        <VideoContainer autoPlay muted playsInline>
-          <source src={VIDEO_SOURCE} type={VIDEO_TYPE} />
-        </VideoContainer>
-      </IntroVideo>
+      <Video autoPlay muted playsInline src={VIDEO_SOURCE()} />
 
       <MainBackground></MainBackground>
 
-      <ParagraphContainer>
-        <FlexColumnCenter>
-          <ParagraphLine1>
-            <span>{t('section1.line1')}</span>
-          </ParagraphLine1>
-          <ParagraphLine2>
-            <span>{t('section1.line2')}</span>
-          </ParagraphLine2>
-        </FlexColumnCenter>
-      </ParagraphContainer>
+      <TextContainer>
+        <Line1 $easeInOut={easeInOut}>
+          <span>{t('section1.line1')}</span>
+        </Line1>
+        <Line2 $easeInOut={easeInOut}>
+          <span>{t('section1.line2')}</span>
+        </Line2>
+      </TextContainer>
     </Container>
   );
 };
@@ -35,26 +36,17 @@ export const Section1 = () => {
 const Container = styled.div`
   width: 100%;
   height: 100vh;
-  background-color: rgb(0, 0, 0);
+  background-color: ${({ theme }) => theme.color.black};
 `;
 
-const IntroVideo = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
+const Video = styled.video`
   width: 100%;
   height: 100%;
-  transform: translate(-50%, -50%);
-  z-index: 100;
-`;
-
-const VideoContainer = styled.video`
-  width: 100%;
-  height: 100%;
+  min-height: 100vh;
   padding-top: 95px;
   object-fit: cover;
 
-  @media (max-width: 768px) {
+  @media ${({ theme }) => theme.media.tabletS} {
     padding-top: 65px;
   }
 `;
@@ -64,7 +56,7 @@ const MainBackground = styled.div`
   top: 95px;
   width: 100%;
   height: calc(100vh - 95px);
-  background-color: linear-gradient(
+  background: linear-gradient(
     0deg,
     rgba(0, 0, 0, 0.75) 0%,
     rgba(0, 0, 0, 0) 100%
@@ -72,7 +64,7 @@ const MainBackground = styled.div`
   z-index: 150;
 `;
 
-const ParagraphContainer = styled.div`
+const TextContainer = styled(FlexColumnCenter)`
   position: absolute;
   left: 50%;
   bottom: 140px;
@@ -81,77 +73,89 @@ const ParagraphContainer = styled.div`
   transform: translate(-50%);
   overflow: hidden;
   z-index: 200;
+
+  @media ${({ theme }) => theme.media.laptop} {
+    bottom: 160px;
+  }
+
+  @media ${({ theme }) => theme.media.tabletS} {
+    padding: 0 60px;
+  }
+
+  @media ${({ theme }) => theme.media.mobile} {
+    bottom: 100px;
+    padding: 0 20px;
+  }
 `;
 
-const ParagraphLine1 = styled.div`
+const Line1 = styled.div<{ $easeInOut: Keyframes }>`
   margin-bottom: 40px;
   opacity: 0;
-  animation: 1s ease-in-out 2.5s 1 normal forwards running easeInOut;
+  ${({ $easeInOut }) =>
+    css`
+      animation: 1s ease-in-out 2.5s 1 normal forwards running ${$easeInOut};
+    `};
+
+  @media ${({ theme }) => theme.media.mobile} {
+    margin-bottom: 20px;
+    ${({ $easeInOut }) =>
+      css`
+        animation: 1s ease-in-out 3.5s 1 normal forwards running ${$easeInOut};
+      `};
+  }
 
   & span {
     display: block;
-    color: rgb(255, 255, 255);
+    color: ${({ theme }) => theme.color.white};
     font-size: 4rem;
     font-weight: 500;
     line-height: 138%;
 
-    @media (max-width: 1280px) {
+    @media ${({ theme }) => theme.media.laptop} {
       font-size: 3.6rem;
     }
 
-    @media (max-width: 768px) {
+    @media ${({ theme }) => theme.media.tabletS} {
       font-size: 2.8rem;
     }
 
-    @media (max-width: 600px) {
+    @media ${({ theme }) => theme.media.mobile} {
       font-size: 1.6rem;
-    }
-  }
-
-  @keyframes easeInOut {
-    from {
-      opacity: 0;
-      transform: translate3d(0, 50px, 0);
-    }
-    to {
-      opacity: 1;
-      transform: translateZ(0);
     }
   }
 `;
 
-const ParagraphLine2 = styled.div`
+const Line2 = styled.div<{ $easeInOut: Keyframes }>`
   opacity: 0;
-  animation: 1s ease-in-out 3s 1 normal forwards running easeInOut;
+  ${({ $easeInOut }) =>
+    css`
+      animation: 1s ease-in-out 3s 1 normal forwards running ${$easeInOut};
+    `};
+
+  @media ${({ theme }) => theme.media.mobile} {
+    ${({ $easeInOut }) =>
+      css`
+        animation: 1s ease-in-out 4s 1 normal forwards running ${$easeInOut};
+      `};
+  }
 
   & span {
     display: block;
-    color: rgb(255, 255, 255);
+    color: ${({ theme }) => theme.color.white};
     font-size: 6rem;
     font-weight: 600;
     line-height: 138%;
 
-    @media (max-width: 1280px) {
+    @media ${({ theme }) => theme.media.laptop} {
       font-size: 4rem;
     }
 
-    @media (max-width: 768px) {
+    @media ${({ theme }) => theme.media.tabletS} {
       font-size: 3.2rem;
     }
 
-    @media (max-width: 600px) {
+    @media ${({ theme }) => theme.media.mobile} {
       font-size: 2rem;
-    }
-  }
-
-  @keyframes easeInOut {
-    from {
-      opacity: 0;
-      transform: translate3d(0, 50px, 0);
-    }
-    to {
-      opacity: 1;
-      transform: translateZ(0);
     }
   }
 `;

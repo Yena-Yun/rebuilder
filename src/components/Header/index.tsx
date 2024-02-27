3abeff7033cca32f802ad59ef76467ccfa91edce
Yena-Yun <cheryleduart@gmail.com>
2024-02-27 19:33:42 +0900

    style: 헤더에 반응형 추가 적용

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -50,23 +50,30 @@ const Container = styled.div`
   width: 100%;
   border-bottom: 1px solid ${({ theme }) => theme.color.black};
   background-color: ${({ theme }) => theme.color.black};
-  z-index: 500;
+  z-index: 999;
 `;
 
 const InnerContainer = styled.div<{
   $isHoverDropdown: boolean;
   $isShowMenu: boolean;
 }>`
-  width: 1200px;
+  width: 100%;
   height: ${({ $isHoverDropdown }) => ($isHoverDropdown ? '181px' : '94px')};
   margin: 0 auto;
   padding: 0 30px;
   transition: all 0.2s ease-in-out 0s;
 
+  @media ${({ theme }) => theme.media.overTabletL} {
+    max-width: 1200px;
+  }
+
   @media ${({ theme }) => theme.media.tabletS} {
-    width: 100%;
     height: ${({ $isShowMenu }) => ($isShowMenu ? '350px' : '64px')};
     padding: 0 60px;
     background-color: ${({ theme }) => theme.color.black};
+  }
+
+  @media ${({ theme }) => theme.media.mobile} {
+    padding: 0 20px;
   }
 `;

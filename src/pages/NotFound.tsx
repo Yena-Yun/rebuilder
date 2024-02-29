00ef1e44f2d2a88e370b80c01ef5c70f406bb8b0
Yena-Yun <cheryleduart@gmail.com>
2024-02-29 14:22:54 +0900

    style: NotFound 페이지 스타일 수정

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -3,24 +3,42 @@ import styled from 'styled-components';
 const NotFound = () => {
   return (
     <Container>
-      <StatusNumber>404</StatusNumber>
-      <br />
-      <ErrorReason>Page Not Found</ErrorReason>
-      <br />
-      <br />
-      <a href='https://rebuilderai.com' aria-label='Go Back to Homepage'>
-        <UserDirection>Get Back to HomePage</UserDirection>
-      </a>
+      <InnerContainer>
+        <NotFoundContent>
+          <StatusNumber>404</StatusNumber>
+          <br />
+          <ErrorReason>Page Not Found</ErrorReason>
+          <br />
+          <br />
+          <a href='https://rebuilderai.com' aria-label='Go Back to Homepage'>
+            <UserDirection>Get Back to HomePage</UserDirection>
+          </a>
+        </NotFoundContent>
+      </InnerContainer>
     </Container>
   );
 };
 
 const Container = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100vh;
+`;
+
+const InnerContainer = styled.div`
+  display: block;
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 16px;
+`;
+
+const NotFoundContent = styled.div`
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   text-align: center;
+  white-space: nowrap;
 `;
 
 const StatusNumber = styled.span`

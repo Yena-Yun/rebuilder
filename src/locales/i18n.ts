10e3dda1c773076661ebf575f68fb7f96fb0b016
Yena-Yun <cheryleduart@gmail.com>
2024-03-18 20:17:41 +0900

    feat: 유저 브라우저의 언어를 기본 언어로 설정

--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -12,9 +12,13 @@ const resources = {
   },
 };
 
+// 유저 브라우저의 언어 확인
+const userLanguage = navigator.language.toLowerCase();
+const isKor = userLanguage.includes('ko'); // 브라우저 공통으로 들어가는 앞 글자 "ko"로 확인
+
 i18n.use(initReactI18next).init({
   resources,
-  lng: 'ko', // 기본 언어 설정
+  lng: isKor ? 'ko' : 'en', // 기본 언어 설정
   fallbackLng: 'en', // 기본 언어가 지원되지 않을 때의 대체 언어 설정
   interpolation: {
     escapeValue: false, // 리액트는 HTML 태그도 렌더링할 수 있어서 escapeValue를 false로 설정
